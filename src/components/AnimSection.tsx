2e79067227262303cc8e350922fce3c869925dc1
Shubham Kapoor <skap0903@gmail.com>
2025-02-04 15:55:24 +0530

    fixed prod error, remove temp

--- src/components/AnimSection.tsx
+++ src/components/AnimSection.tsx
@@ -7,7 +7,7 @@ const AnimSection = () => {
   return (
     <div className="w-full px-8 flex h-[30em] gap-4 ">
       <div className="section1 w-2/3">
-        <div className="w-full h-full flex flex-row items-start justify-start bg-white text-foreground dark:text-muted font-normal overflow-hidden lg:p-16 lg:pt-48">
+        <div className="w-full h-full flex flex-row items-start justify-start bg-white text-foreground dark:text-muted font-normal overflow-hidden lg:p-16 lg:pt-48 text-bold">
           <p className="whitespace-pre-wrap text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
             <span>{"We're here to help you "}</span>
             <Typewriter
